871160e3ed39fdfa5a4d362c9029d23f27ae810c
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-03-02 12:44:22 +0530

    New UI
    Changed Whole UI also fixed unknown, upgraded jQuery, switched to bootstrap 5.3.0, fixed not checking all cc.

--- index.php
+++ index.php
@@ -1,89 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+ <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="title" content="CHECKER CC">
-  <meta name="description" content="CC checker is a tool that checks if a credit card is correct. It not only checks the validity but also whether the credit card is alive or dead.">
-  <meta name="keywords" content="cc, checker, mass cc checker, bin, generated cc, credit card, validator, free, live, dead, cvv, cvc, amex, master, platinum, debit card, php, awesome, oshekher, puritywashere, cracking, hacking, carding, trial, source, website, 18+, OshekharO, Saksham Shekher, feilu, alternative">
-  <meta name="robots" content="index, follow">
-  <meta name="language" content="English">
-  <meta name="author" content="Saksham">
-  <meta property="og:url" content="/">
-  <meta property="og:image" content="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/apple-touch-icon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="title" content="CC Checker - Free credit card checker namso-gen.eu.org" />
+  <meta name="description" content="cc checker for free credit card numbers." />
+  <meta name="keywords" content="cc,checker,mass cc checker,bin,generated cc,credit card,live,dead" />
+  <meta name="robots" content="index, follow" />
+  <meta name="language" content="English" />
+  <meta name="author" content="Saksham" />
+  <meta property="og:url" content="https://namso-gen.eu.org" />
+  <meta property="og:image" content="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/apple-touch-icon.png" />
   <meta property="og:locale" content="en_US" />
   <meta property="og:type" content="website" />
-  <meta name="copyright" content="Copyright © 2022 OshekharO" />
+  <meta name="copyright" content="Copyright © 2023 OshekharO" />
   <meta property="og:image" content="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/android-chrome-512x512.png" />
   <link rel="shortcut icon" href="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/favicon.ico" type="image/x-icon" />
-  <link rel="apple-touch-icon" sizes="180x180" href="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/apple-touch-icon.png">
-  <link rel="icon" type="image/png" href="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="https://github.com/OshekharO/Entertainment-Index/blob/master/img/favicon-16x16.png" sizes="16x16">
-<title>CHECKER CC</title>
-<link href="style.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Bungee" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
-</head>
+  <link rel="apple-touch-icon" sizes="180x180" href="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" href="https://rawcdn.githack.com/OshekharO/Entertainment-Index/17d005915d5e20780a46aef227f08367ca8efb3a/img/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="https://github.com/OshekharO/Entertainment-Index/blob/master/img/favicon-16x16.png" sizes="16x16" />
+  <title>CHECKER CC</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+  <link href="style.css" rel="stylesheet" />
+ </head>
 
-<body>
-<p class="head">Credit Card Checker</p>
+ <body>
+  <div class="fs-3 fw-bold mb-5 text-uppercase mx-auto text-center text-light">Credit Card Checker</div>
+  <form method="post" action="api.php" role="form" id="form">
+   <div class="box-body">
+    <div class="box-content">
+     <label for="cc" class="form-label fs-6 font-monospace badge bg-danger text-light">Card Numbers</label>
+     <div>
+      <textarea class="form-control" rows="10" id="cc" name="cc" title="53012724539xxxxx|05|2022|653" placeholder="53012724539xxxxx|05|2022|653" required></textarea>
+     </div>
+     <div class="button text-center mb-3 mt-3">
+      <button type="submit" name="valid" class="btn btn-outline-success text-light">START</button>
+      <button type="button" id="stop" class="btn btn-outline-danger text-light" disabled>STOP</button>
+     </div>
+    </div>
+   </div>
 
-<form method="post" action="api.php" role="form" id="form">
-<div class="alert alert-danger text-center info" style="display: none"></div>
-<div class="box-title"><h1>Card Checker</h1></div>
-<div class="box-body">
-<div class="box-content">
-<label>Card Numbers</label>
-<div><textarea rows="10" id="cc" name="cc" title="53012724539xxxxx|05|2022|653" placeholder="53012724539xxxxx|05|2022|653" required="required">
-</textarea></div>
-<div>
-<button type="submit" name="valid" class="btn btn-success btn-lg btn-block">Start</button>
-<button type="button" id="stop" class="btn btn-danger btn-lg btn-block" disabled="disabled">Stop</button>
-</div>
-</div>
-</div>
+   <!-- Info success -->
+   <div class="box-title">
+    <h3 class="panel-title alert alert-primary font-monospace">Live - <span class="badge bg-success live">0</span></h3>
+   </div>
+   <div class="box-body">
+    <div class="box-content alert alert-success">
+     <div class="panel-body success"></div>
+    </div>
+   </div>
 
-<div style="margin:10px;"></div>
+   <!-- Info error -->
+   <div class="box-title">
+    <h3 class="panel-title alert alert-primary font-monospace">Die - <span class="badge bg-danger die">0</span></h3>
+   </div>
+   <div class="box-body">
+    <div class="box-content alert alert-danger">
+     <div class="panel-body danger"></div>
+    </div>
+   </div>
+   
+    <!-- Info unknown -->
+      <div class="box-title">
+      <h3 class="panel-title alert alert-primary font-monospace">Unknown - <span class="badge bg-warning unknown">0</span></h3>
+      </div>
+      <div class="box-body">
+        <div class="box-content alert alert-warning">
+          <div class="panel-body warning"></div>
+        </div>
+      </div>
+  </form>
 
-<!-- Info success -->
-<div class="box-title-success"><h3 class="panel-title">Live - <span class="badge live">0</span></h3></div>
-<div class="box-body">
-<div class="box-content">
-<div class="panel-body success">
-</div>
-</div>
-</div>
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 
-<div style="margin:10px;"></div>
-
-<!-- Info error -->
-<div class="box-title-danger"><h3 class="panel-title">Die - <span class="badge die">0</span></h3></div>
-<div class="box-body">
-<div class="box-content">
-<div class="panel-body danger">
-</div>
-</div>
-</div>
-
-<div style="margin:10px;"></div>
-
-<!-- Info unknown -->
-<div style="display: none;">
-
-<div class="box-title-warning"><h3 class="panel-title">Unknown - <span class="badge unknown">0</span></h3></div>
-<div class="box-body">
-<div class="box-content">
-<div class="panel-body warning">
-</div>
-</div>
-</div>
-
-</div>
-
-</form>
-<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script><script>window.jQuery || document.write('<script src="http://gatortools.tk/tools/checker/assets/jquery.min.js"><\/script>')</script><script type="text/javascript">var _0x1c00=["\x64\x69\x73\x61\x62\x6C\x65\x64","\x61\x74\x74\x72","\x62\x75\x74\x74\x6F\x6E\x5B\x6E\x61\x6D\x65\x3D\x22\x76\x61\x6C\x69\x64\x22\x5D","\x62\x75\x74\x74\x6F\x6E\x5B\x69\x64\x3D\x22\x73\x74\x6F\x70\x22\x5D","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x73\x74\x6F\x70\x49\x6D\x6D\x65\x64\x69\x61\x74\x65\x50\x72\x6F\x70\x61\x67\x61\x74\x69\x6F\x6E","\x0A","\x73\x70\x6C\x69\x74","\x76\x61\x6C","\x23\x63\x63","","\x6F\x62\x6A\x65\x63\x74","\x74\x65\x78\x74","\x2E\x6C\x69\x76\x65","\x2E\x64\x69\x65","\x2E\x75\x6E\x6B\x6E\x6F\x77\x6E","\x6C\x65\x6E\x67\x74\x68","\x61\x63\x74\x69\x6F\x6E","\x73\x75\x63\x63\x65\x73\x73","\x70\x61\x72\x73\x65\x4A\x53\x4F\x4E","\x65\x72\x72\x6F\x72","\x6D\x73\x67","\x70\x72\x65\x70\x65\x6E\x64","\x2E\x73\x75\x63\x63\x65\x73\x73","\x2E\x64\x61\x6E\x67\x65\x72","\x2E\x77\x61\x72\x6E\x69\x6E\x67","\x3C\x62\x72\x3E","\x73\x68\x6F\x77","\x2E\x69\x6E\x66\x6F","\x70\x6F\x73\x74","\x3C\x62\x3E\x45\x72\x72\x6F\x72\x3C\x2F\x62\x3E","\x68\x74\x6D\x6C","\x73\x75\x62\x6D\x69\x74","\x23\x66\x6F\x72\x6D","\x63\x6C\x69\x63\x6B","\x23\x73\x74\x6F\x70","\x72\x65\x61\x64\x79"];$(document)[_0x1c00[36]](function(){$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],true);var _0x9475x1;$(_0x1c00[33])[_0x1c00[32]](function(_0x9475x2){_0x9475x2[_0x1c00[4]]();_0x9475x2[_0x1c00[5]]();var _0x9475x3=$(this);var _0x9475x4=$(_0x1c00[9])[_0x1c00[8]]()[_0x1c00[7]](_0x1c00[6]);if(_0x9475x4!= _0x1c00[10]|| typeof _0x9475x4!= _0x1c00[11]){var _0x9475x5=0,_0x9475x6=0+ $(_0x1c00[13])[_0x1c00[12]](),_0x9475x7=0+ $(_0x1c00[14])[_0x1c00[12]](),_0x9475x8=0+ $(_0x1c00[15])[_0x1c00[12]](),_0x9475x9=_0x9475x4[_0x1c00[16]];_0x9475x1= setInterval(function(){$[_0x1c00[29]](_0x9475x3[_0x1c00[1]](_0x1c00[17]),{"\x64\x61\x74\x61":_0x9475x4[_0x9475x5]},function(_0x9475xa,_0x9475xb){if(_0x9475xb== _0x1c00[18]){var _0x9475xc=$[_0x1c00[19]](_0x9475xa);if(_0x9475xc[_0x1c00[20]]== 1){$(_0x1c00[23])[_0x1c00[22]](_0x9475xc[_0x1c00[21]]);_0x9475x6++;$(_0x1c00[13])[_0x1c00[12]](_0x9475x6)}else {if(_0x9475xc[_0x1c00[20]]== 2){$(_0x1c00[24])[_0x1c00[22]](_0x9475xc[_0x1c00[21]]);_0x9475x7++;$(_0x1c00[14])[_0x1c00[12]](_0x9475x7)}else {if(_0x9475xc[_0x1c00[20]]== 3){$(_0x1c00[25])[_0x1c00[22]](_0x9475xc[_0x1c00[21]]);_0x9475x8++;$(_0x1c00[15])[_0x1c00[12]](_0x9475x8)}else {if(_0x9475xc[_0x1c00[20]]== 4){$(_0x1c00[28])[_0x1c00[27]]()[_0x1c00[22]](_0x9475xc[_0x1c00[21]]+ _0x1c00[26])}}}}}});if(_0x9475x5== _0x9475x9){clearInterval(_0x9475x1);$(_0x1c00[9])[_0x1c00[8]](_0x1c00[10]);$(_0x1c00[9])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],true)}else {_0x9475x5++;$(_0x1c00[9])[_0x1c00[1]](_0x1c00[0],true);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],true)}},1500)}else {$(_0x1c00[28])[_0x1c00[27]]()[_0x1c00[31]](_0x1c00[30])};return false});$(_0x1c00[35])[_0x1c00[34]](function(){clearInterval(_0x9475x1);$(_0x1c00[9])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],true)})})</script>
-</body>
-
+<script type="text/javascript">var _0x1c00=["\x64\x69\x73\x61\x62\x6C\x65\x64","\x61\x74\x74\x72","\x62\x75\x74\x74\x6F\x6E\x5B\x6E\x61\x6D\x65\x3D\x22\x76\x61\x6C\x69\x64\x22\x5D","\x62\x75\x74\x74\x6F\x6E\x5B\x69\x64\x3D\x22\x73\x74\x6F\x70\x22\x5D","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x73\x74\x6F\x70\x49\x6D\x6D\x65\x64\x69\x61\x74\x65\x50\x72\x6F\x70\x61\x67\x61\x74\x69\x6F\x6E","\x0A","\x73\x70\x6C\x69\x74","\x76\x61\x6C","\x23\x63\x63","","\x6F\x62\x6A\x65\x63\x74","\x74\x65\x78\x74","\x2E\x6C\x69\x76\x65","\x2E\x64\x69\x65","\x2E\x75\x6E\x6B\x6E\x6F\x77\x6E","\x6C\x65\x6E\x67\x74\x68","\x61\x63\x74\x69\x6F\x6E","\x73\x75\x63\x63\x65\x73\x73","\x70\x61\x72\x73\x65\x4A\x53\x4F\x4E","\x65\x72\x72\x6F\x72","\x6D\x73\x67","\x70\x72\x65\x70\x65\x6E\x64","\x2E\x73\x75\x63\x63\x65\x73\x73","\x2E\x64\x61\x6E\x67\x65\x72","\x2E\x77\x61\x72\x6E\x69\x6E\x67","\x3C\x62\x72\x3E","\x73\x68\x6F\x77","\x2E\x69\x6E\x66\x6F","\x70\x6F\x73\x74","\x3C\x62\x3E\x45\x72\x72\x6F\x72\x3C\x2F\x62\x3E","\x68\x74\x6D\x6C","\x73\x75\x62\x6D\x69\x74","\x23\x66\x6F\x72\x6D","\x63\x6C\x69\x63\x6B","\x23\x73\x74\x6F\x70","\x72\x65\x61\x64\x79"];$(document)[_0x1c00[36]](function(){$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],true);var _0x9475x1;$(_0x1c00[33])[_0x1c00[32]](function(_0x9475x2){_0x9475x2[_0x1c00[4]]();_0x9475x2[_0x1c00[5]]();var _0x9475x3=$(this);var _0x9475x4=$(_0x1c00[9])[_0x1c00[8]]()[_0x1c00[7]](_0x1c00[6]);if(_0x9475x4!= _0x1c00[10]|| typeof _0x9475x4!= _0x1c00[11]){var _0x9475x5=0,_0x9475x6=0+ $(_0x1c00[13])[_0x1c00[12]](),_0x9475x7=0+ $(_0x1c00[14])[_0x1c00[12]](),_0x9475x8=0+ $(_0x1c00[15])[_0x1c00[12]](),_0x9475x9=_0x9475x4[_0x1c00[16]];_0x9475x1= setInterval(function(){$[_0x1c00[29]](_0x9475x3[_0x1c00[1]](_0x1c00[17]),{"\x64\x61\x74\x61":_0x9475x4[_0x9475x5]},function(_0x9475xa,_0x9475xb){if(_0x9475xb== _0x1c00[18]){var _0x9475xc=$[_0x1c00[19]](_0x9475xa);if(_0x9475xc[_0x1c00[20]]== 1){$(_0x1c00[23])[_0x1c00[22]](_0x9475xc[_0x1c00[21]]);_0x9475x6++;$(_0x1c00[13])[_0x1c00[12]](_0x9475x6)}else {if(_0x9475xc[_0x1c00[20]]== 2){$(_0x1c00[24])[_0x1c00[22]](_0x9475xc[_0x1c00[21]]);_0x9475x7++;$(_0x1c00[14])[_0x1c00[12]](_0x9475x7)}else {if(_0x9475xc[_0x1c00[20]]== 3){$(_0x1c00[25])[_0x1c00[22]](_0x9475xc[_0x1c00[21]]);_0x9475x8++;$(_0x1c00[15])[_0x1c00[12]](_0x9475x8)}else {if(_0x9475xc[_0x1c00[20]]== 4){$(_0x1c00[28])[_0x1c00[27]]()[_0x1c00[22]](_0x9475xc[_0x1c00[21]]+ _0x1c00[26])}}}}}});if(_0x9475x5== _0x9475x9){clearInterval(_0x9475x1);$(_0x1c00[9])[_0x1c00[8]](_0x1c00[10]);$(_0x1c00[9])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],true)}else {_0x9475x5++;$(_0x1c00[9])[_0x1c00[1]](_0x1c00[0],true);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],true)}},1500)}else {$(_0x1c00[28])[_0x1c00[27]]()[_0x1c00[31]](_0x1c00[30])};return false});$(_0x1c00[35])[_0x1c00[34]](function(){clearInterval(_0x9475x1);$(_0x1c00[9])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[2])[_0x1c00[1]](_0x1c00[0],false);$(_0x1c00[3])[_0x1c00[1]](_0x1c00[0],true)})})</script>
+ </body>
 </html>
